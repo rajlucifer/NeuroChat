0eacdbdb670f0887f9adfb924e935857199795b6
rahul raj <thisisrajtkd18@gmail.com>
2026-08-03 16:03:02 +0530

    profile page work is done

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import assets from '../assets/assets'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
+  const [selectedImg,setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name,setName] = useState("Martin jhonson");
+  const [bio,setBio] = useState("Hi everyone i am using Quick chat");
+  const submitHandler = async (e)=>{
+    e.preventDefault;
+    //after sumbit it goes to home page
+    navigate("/")
+
+  }
+    return (
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center   '>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-500 border-2 border-gray-500  flex items-center justify-between  max-sm:flex-col-reverse rounded-lg '>
+        <form onSubmit={submitHandler} className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className=' text-white text-lg '>Profile Details</h3>
+          <label htmlFor='avatar' className='flex items-center gap-3 cursor-pointer'>
+            <input onChange={(e)=>setSelectedImg(e.target.files[0])} type='file' id="avatar"  accept='.png .jpeg .jpg' hidden />
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon }  className={`w-12 h-12  object-cover rounded-full  ${selectedImg && 'rounded-full'} `}>
+            </img>
+            Upload Image Profile
+             
+          </label>
+          <input onChange={(e)=>setName(e.target.value)} type='text' placeholder='Enter Your Name'  value={name} required className='bg-transparent  outline-none  p-2 border border-gray-500  ring-2 ring-violet-400 rounded-md' />
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} rows={3} placeholder='Write Profile Bio'  required className='bg-transparent  outline-none  p-2 border border-gray-500  ring-2 ring-violet-400 rounded-md'> </textarea>
+          <button type='submit' className='p-2 bg-gradient-to-r from-purple-400 to-violet-600 rounded-full'>Save</button>
+
+
+        </form>
+        <img src={assets.logo_icon} alt='logo-icon' className='max-w-44  aspect-square rounded-full mx-10 max-sm:mt-10' ></img>
+      </div>
+       
+    </div>
   )
 }
