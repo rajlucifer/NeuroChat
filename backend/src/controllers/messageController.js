1da871c29e550934005bb59867cdd9ef9cada7bc
rahul raj <thisisrajtkd18@gmail.com>
2026-08-05 17:47:45 +0530

    make the messageController for message handling

---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,119 @@
+import MessageModel from "../models/message.js";
+import UserModel from "../models/Users.js";
+
+
+// get all user except logged in user
+export const getUsersForSiderBar = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const filterUser = await UserModel.find({ _id: { $ne: userId } }).select("-password");
+
+
+        // count number of message not seen 
+        const unseenMessage = {};
+        const promises = filterUser.map(async(user)=>{
+            const messages = await MessageModel.find({senderId:user._id,receiverId:userId,seen:false});
+            if(messages.length > 0){
+                // by doing this we can create the key value pair of object
+                unseenMessage[user._id] = messages.length;
+            }
+
+            
+        })
+        await Promise.all(promises);
+        res.json({
+            success:true,
+            users:filterUser,
+            unseenMessage,
+        })
+
+
+
+    }
+    catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+};
+
+
+
+//get message for selected user
+
+export const getMessages = async(req,res)=>{
+    try{
+        const{ id:selectedUserId } = req.params;
+        const myId = req.user._id;
+        const messages = await MessageModel.find({
+            $or:[
+                {senderId:myId,receiverId:selectedUserId},
+                {senderId:selectedUserId,receiverId:myId}
+            ]
+        });
+        await MessageModel.updateMany({senderId:selectedUserId,receiverId:myId},{seen:true});
+        res.json({
+            success:true,
+            messages
+        })
+
+
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+
+    }
+};
+
+
+//api to mark message as seen using message id
+
+export const markMessageSeen = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        await MessageModel.findByIdAndUpdate({id},{seen:true});
+        res.json({
+            success:true,
+            
+        })
+
+
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+
+    }
+};
+
+
+//send message to selected user
+
+export const sendMessage = async(req,res)=>{
+    try{
+        const {text,image} =req.body;
+        const receiverId =  req.params.id;
+        // we get the sendId which is we get from the protected routes
+        const senderId = req.user._id;
+
+
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+
+    }
+
+}
